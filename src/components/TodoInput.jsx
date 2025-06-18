30b14f0d5e46afd8674136bd5e6cc3bd5cd3c3b8
ibrahiimnawzad <ibrahiimnawzad@gmail.com>
2025-06-18 14:50:52 +0300

    new

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,22 @@
+
+export default function TodoInput(props) {
+    const { handleAddTodos, todoValue, setTodoValue } = props
+
+    const addTask = () => {
+        handleAddTodos(todoValue)
+        setTodoValue('')
+    }
+
+    return (
+        <header>
+            <form onSubmit={addTask}>
+                <input value={todoValue} onChange={(e) => {
+                    setTodoValue(e.target.value)
+                }} placeholder="Enter todo..." />
+
+                <button disabled={todoValue === '' ? true : false} onClick={addTask}>Add</button>
+
+            </form>
+        </header>
+    )
+}
